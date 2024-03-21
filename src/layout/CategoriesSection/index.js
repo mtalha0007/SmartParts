d51efbdb18ee7e2950dbde49f2c4551b3fefab1a
mtalha0007 <mtalha.mangotech@gmail.com>
2024-03-21 16:40:51 +0500

    changes in Categories

--- src/layout/CategoriesSection/index.js
+++ src/layout/CategoriesSection/index.js
@@ -6,13 +6,13 @@ import PopularCategories from '../../components/Categories/PopularCategories'
 export default function index() {
   return (
     <Box sx={{display:"flex",gap:"15px" ,padding:"10px"}}>
-        <Box sx={{display: {sm: 'none' ,xs:"none" ,lg:"flex"}}}><TrendCategories/></Box>
+        <Box sx={{display: {sm: 'none' ,xs:"none" ,md:"flex"}}}><TrendCategories/></Box>
         <Box sx={{
-          width:"80%",
+          width:"76%",
           margin:"0 auto" ,
           display:"flex",
           flexDirection:"column",
-          '@media screen and (max-width:1030px)':{
+          '@media screen and (max-width:900px)':{
             width:"100%",
         }}}>
           <PopularCategories/>
